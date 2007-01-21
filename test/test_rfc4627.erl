21662f3d3c133b936bdee731d99e624f25830b7d
Tony Garnock-Jones <tonyg@kcbbs.gen.nz>
2007-01-21 05:20:36 +0000

    [project @ Test suite.]

---
+++ test/test_rfc4627.erl
@@ -0,0 +1,50 @@
+-module(test_rfc4627).
+-include_lib("rfc4627/include/rfc4627.hrl").
+
+-export([test_all/0]).
+
+-record(address, {number, street, town}).
+
+test_all() ->
+    passed = test_codec(),
+    passed = test_records(),
+    passed.
+
+test_codec() ->
+    Cases = [
+	     {1, "1"},
+	     {1, "1,", false},
+	     {1.0, "1.0"},
+	     {12.3, "1.23e1"},
+	     {<<"hi">>, "\"hi\""},
+	     {<<>>, "\"\""},
+	     {[1, 2], "[1, 2]"},
+	     {[], "[]"},
+	     {{obj, [{"a", 1}, {"b", 2}]}, "{\"a\": 1, \"b\": 2}"},
+	     {{obj, []}, "{}"},
+	     {{obj, []}, "{    \n\n  }"},
+	     {true, "true"},
+	     {null, "null"},
+	     {false, "false"}
+	     ],
+    lists:foreach(fun test_codec/1, Cases),
+    passed.
+
+test_codec({Erl, Json}) ->
+    test_codec({Erl, Json, true});
+test_codec({Erl, Json, EofExpected}) ->
+    %% We can test Erl -> Json -> Erl, but not Json -> Erl ->
+    %% Json. However, we can test Json -> Erl.
+    {ok, Erl, []} = rfc4627:decode(rfc4627:encode(Erl)),
+    {ok, Erl, Rest} = rfc4627:decode(Json),
+    {at_eof, EofExpected} = {at_eof, (Rest == [])},
+    passed.
+
+test_records() ->
+    A = #address{number = 6, street = <<"Rufus Street">>, town = <<"London">>},
+    AEnc = {obj, [{"number", 6},
+		  {"street", <<"Rufus Street">>},
+		  {"town", <<"London">>}]} = ?RFC4627_FROM_RECORD(address, A),
+    A = ?RFC4627_TO_RECORD(address, AEnc),
+    {ok, AEnc, []} = rfc4627:decode(rfc4627:encode(AEnc)),
+    passed.
